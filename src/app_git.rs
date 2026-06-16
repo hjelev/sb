329618b0dae6c948bbcc53b7134acbf997552582
Hristo Jelev <jeleff@gmail.com>
2026-06-16 19:04:25 +0300

    Improve the git workflow to do a pull before pushing

--- src/app_git.rs
+++ src/app_git.rs
@@ -278,6 +278,13 @@ impl App {
             }
         }
 
+        if failed_step.is_none() && !amend {
+            println!("$ git pull --rebase");
+            if !run_step(&["pull", "--rebase"], &self.current_dir)? {
+                failed_step = Some("git pull --rebase failed (resolve conflicts manually)".to_string());
+            }
+        }
+
         if failed_step.is_none() {
             if amend {
                 println!("$ git push origin HEAD -f");
